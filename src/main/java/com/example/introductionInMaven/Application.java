ab439564fa1d59b0ef64460e2c2786d4e4218b6d
Максим <gmax7911@gmail.com>
2023-02-17 19:27:30 +0300

    Привет

---
+++ src/main/java/com/example/introductionInMaven/Application.java
@@ -0,0 +1,13 @@
+package com.example.introductionInMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/introductionInMaven/controllers/Controller.java
@@ -0,0 +1,20 @@
+package introductionInMaven.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping
+    public String appRun() {
+        return "Приложение запущено";
+    }
+
+    @GetMapping("/info")
+    public String info(){
+        return "Имя ученика: Максим Анатольевич Герасименко \n Название проекта: Введение в Maven \n " +
+                "Дата создания проекта: 17.02.2023" +
+                "\n Описание проекта:  В рамкад данного курса будет создано WEB приложение для сайта рецептов";
+    }
+
+}
